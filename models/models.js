1a5fd87442a8ae2d85a48da873678c651e4939ed
mmoontoro <montoromendoza.mariano@gmail.com>
2015-05-11 05:44:54 -0700

    Despliegue DB Heroku

--- models/models.js
+++ models/models.js
@@ -1,30 +1,33 @@
 var path = require('path');
+var pg =require('pg');
 
-//CARGAR modelo ORM
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 var Sequelize = require('sequelize');
 
-//USAR BBDD SQlite o  Postgres
-var sequelize = new Sequelize(null, null, null,
-			{dialect: "sqlite",
-                        storage: "quiz.sqlite",
-                    }
-                );
+var sequelize = new Sequelize(DB_name, user, pwd, 
+		{dialect: protocol, protocol: protocol, port: port, host: host, storage: storage, omitNull: true});
 
-//Importar la definicion de la tabla quiz en quiz.js
-var quiz_path = path.join(__dirname,'quiz');
+var quiz_path=path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(quiz_path);
+exports.Quiz = Quiz;
 
-exports.Quiz = Quiz; //exportar definicion de la tabla Quiz
-
-//sequelize.sync() crea e inicializa la tabla de preguntas en DB
 sequelize.sync().then(function() {
-    //then(..) ejecuta el manejador una vez creada la tabla
-    Quiz.count().then(function(count){
-        if(count === 0) { //la tabla se inicializa solo si esta vacia
-            Quiz.create({ pregunta:'Capital de Italia',
-                            respuesta: 'Roma'
-                        })
-            .then( function(){console.log('Base de datos inicializa')});
-        };
-    });
+	Quiz.count().then(function (count) {
+		if( count == 0 ) {
+			Quiz.create({pregunta: 'Capital de Italia',
+					respuesta: 'Roma'});
+			Quiz.create({pregunta: 'Capital de Portugal',
+					respuesta: 'Roma'}).then(function() { console.log('Base de datos inicializada')});
+		};
+	});
 });
+
