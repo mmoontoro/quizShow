50abac74d3007ff764ff88c2cbc0e8e645270aa2
mmoontoro <montoromendoza.mariano@gmail.com>
2015-05-17 15:56:41 -0700

    Crear Comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,7 +1,7 @@
 var models= require('../models/models.js');
 // Autoload :id
 exports.load = function(req, res, next, quizId) {
-	models.Quiz.find(quizId).then(function(quiz) {
+	models.Quiz.find({where: { id: Number(quizId) }, include: [{model: models.Comment}]}).then(function(quiz) {
 	if (quiz) {
 	req.quiz = quiz;
 	next();
