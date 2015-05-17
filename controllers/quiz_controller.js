03d3a7c7bf7042be757bf8b7a77cee108f248b54
mmoontoro <montoromendoza.mariano@gmail.com>
2015-05-17 11:08:52 -0700

    Crear Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -37,4 +37,24 @@ exports.answer = function(req, res){
 	}
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
+exports.new = function(req, res){
+	var quiz = models.Quiz.build( //crea objeto quiz
+	{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new', {quiz: quiz, errors: []});
+};
+//POST /quizes/create
+exports.create = function(req,res){
+	var quiz = models.Quiz.build(req.body.quiz);
+	quiz.validate().then(function(err){
+	if (err) {
+		res.render('quizes/new', {quiz: quiz, errors: err.errors});
+	} else {
+		quiz // save: guarda en DB campos pregunta y respuesta de quiz
+		.save({fields: ["pregunta", "respuesta" ]})
+		.then( function(){ res.redirect('/quizes')})
+	} // res.redirect: Redirección HTTP a lista de preguntas
+	}
+	);
+};
 	
--- routes/index.js~
+++ routes/index.js~
@@ -6,7 +6,7 @@ var quizController=require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz Show' });
 });
-
+router.param('quizId', quizController.load);
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
