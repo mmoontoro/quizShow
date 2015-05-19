fae47d9554f702b02384e2cce642d08bc0a1fa9c
mmoontoro <montoromendoza.mariano@gmail.com>
2015-05-19 04:59:02 -0700

    Gestion usuarios

--- models/models.js
+++ models/models.js
@@ -22,20 +22,37 @@ var Quiz = sequelize.import(quiz_path);
 var comment_path=path.join(__dirname, 'comment');
 var Comment = sequelize.import(comment_path);
 
+// Importar definicion de la tabla Comment
+var user_path = path.join(__dirname,'user');
+var User = sequelize.import(user_path);
+
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz;
 exports.Comment = Comment;
+exports.User = User;
+
 
 sequelize.sync().then(function() {
-	Quiz.count().then(function (count) {
-		if( count == 0 ) {
-			Quiz.create({pregunta: 'Capital de Italia',
-					respuesta: 'Roma'});
-			Quiz.create({pregunta: 'Capital de Portugal',
-					respuesta: 'Lisboa'}).then(function() { console.log('Base de datos inicializada')});
-		};
-	});
+User.count().then(function (count){
+	if(count === 0) { // la tabla se inicializa solo si está vacía
+		User.bulkCreate(
+		[ {username: 'admin', password: '1234', isAdmin: true},
+		{username: 'pepe', password: '5678'} // el valor por defecto de isAdmin es 'false'
+		]
+		 ).then(function(){
+		 console.log('Base de datos (tabla user) inicializada');
+		 Quiz.count().then(function (count){
+			 if(count === 0) { // la tabla se inicializa solo si está vacía
+				 Quiz.bulkCreate(
+				 [ {pregunta: 'Capital de Italia', respuesta: 'Roma', UserId: 2}, // estos quizes pertenecen al usuario pepe
+				 {pregunta: 'Capital de Portugal', respuesta: 'Lisboa', UserId: 2}
+				 ]
+				 ).then(function(){console.log('Base de datos (tabla quiz) inicializada')});
+			 };
+		 });
+		 });
+	};
 });
-
+}); 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var quizController=require('../controllers/quiz_controller');
 var commentController=require('../controllers/comment_controller');
 var sessionController=require('../controllers/session_controller');
+var userController=require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,11 +11,18 @@ router.get('/', function(req, res) {
 });
 router.param('quizId', quizController.load);
 router.param('commentId', commentController.load);
+router.param('userId', userController.load);
 
 
 router.get('/login', sessionController.new);
 router.post('/login', sessionController.create);
 router.get('/logout', sessionController.destroy);
+
+router.get('/user', userController.new); // formulario sign un
+router.post('/user', userController.create); // registrar usuario
+router.get('/user/:userId(\\d+)/edit', sessionController.loginRequired, userController.edit); // editar información de cuenta
+router.put('/user/:userId(\\d+)', sessionController.loginRequired, userController.update); // actualizar información de cuenta
+router.delete('/user/:userId(\\d+)', sessionController.loginRequired, userController.destroy);
 
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
